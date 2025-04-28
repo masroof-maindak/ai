fix(nn): sigmoid derivative

nn/Lab7/activation_fns.py
@@ -2,12 +2,9 @@ import numpy as np
 
 
 def sigmoid(x, derivative=False) -> np.ndarray:
-    if derivative:
-        return x * (1 - x)
-    return 1 / (1 + np.exp(-x))  # f(x) = 1 / 1 + e^-x
+    sig = 1 / (1 + np.exp(-x))  # f(x) = 1 / 1 + e^-x
+    return sig if not derivative else sig * (1 - sig)
 
 
 def relu(Z: np.ndarray, derivative=False):
-    if derivative:
-        return Z > 0
-    return np.maximum(0, Z)  # max(0, x)
+    return Z > 0 if derivative else np.maximum(0, Z)

nn/Lab7/main.py
@@ -41,6 +41,8 @@ OUTPUT_SIZE = 1
 
 class NeuralNetwork:
     def __init__(self):
+        np.random.seed(1)
+
         # Input layer (8 features) →  Hidden layer (4 neurons)
         self.W1 = np.random.randn(INPUT_SIZE, HIDDEN_SIZE)
         self.b1 = np.ones((1, HIDDEN_SIZE))
@@ -97,9 +99,14 @@ class NeuralNetwork:
         # activations of prior layer (transposed) -> 4 x NUM_SAMPLES
         d_z2_W2 = self.A1.T
 
-        tmp = (self.a2.flatten() - y_real).reshape(-1, 1)
-        common1 = tmp * actvns.sigmoid(self.a2, derivative=True)
-        # common1 = d_loss_a2 * d_a2_z2
+        """
+        NOTE: uncomment out the two lines below and comment the line below them
+        to instead get an elegant evaluation of the BCE loss' derivative specifically
+        when it is applied in tandem with 
+        """
+        # tmp = (self.a2.flatten() - y_real).reshape(-1, 1)
+        # common1 = tmp * actvns.sigmoid(self.a2, derivative=True)
+        common1 = d_loss_a2 * d_a2_z2
 
         # How sensitive the cost function is with respect to the weights/biases
         d_loss_W2 = (d_z2_W2 @ common1) / m